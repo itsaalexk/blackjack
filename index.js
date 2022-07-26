0244c3e6c4d047a0b843a7bf10d25f4e3cb6fd2c
itsaalexk <alexkononenko6@protonmail.com>
2022-07-26 09:49:42 +0200

    pedir carta

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let deck = [];
 const tipos= ["C","D","H","S"]
 const tiposEspeciales = ["A","J","Q","K"];
+let puntos = document.querySelector("small")
+const btnCarta = document.querySelector("#pedir")
+let puntosJugador = 0;
+    puntosPc      = 0;
 
+    // Crea la baraja de cartas
 const createDeck = ()=>{
 
     for (let i = 2; i<= 10 ; i++){
         for (tipo of tipos ){
-            deck.push( i +tipos)
+            deck.push( i +tipo)
         }
     }
     for (tipo of tipos){
@@ -16,24 +21,43 @@ const createDeck = ()=>{
     }
     
     //utilizando la libreria underscore, baraja las cartas que hemos creado
+    
     deck = _.shuffle(deck)
     return deck
 }
-
 createDeck()
+
+
+// Pide la baraja de cartas
+
 
 const pedirCarta= (card) =>{
     if (deck.lenght === 0){
-        throw new Error ("No quedan cartas disponibles")
+        throw ("No quedan cartas disponibles")
     }
     let carta = deck.pop()
     return carta;    
 }
-pedirCarta()
 
-const valorCarta = (card) =>{
-    const valor = card.substring(0, card.length-1)
-    if ( isNaN(card))
-    console.log(valor)
+
+const valorCarta = (carta) =>{
+    const valor = carta.substring(0,carta.length-1);
+    return ( isNaN( valor )) ?
+            (valor === "A") ? 11 : 10
+            : valor *1;
+    
+    }
+
+
+
+    //EVENTOS
+
+btnCarta.onclick = ()=>{
+    const carta = pedirCarta();
+    console.log(carta)
+    puntosJugador = puntosJugador + valorCarta(carta)
+    
+    
+    puntos.textContent = puntosJugador
+    
 }
-valorCarta("2D")
