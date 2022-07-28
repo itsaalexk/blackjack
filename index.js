162b7053e9695fde9e02036f7d969508a012f23e
itsaalexk <alexkononenko6@protonmail.com>
2022-07-28 10:28:07 +0200

    añadido patron módulo

--- index.js
+++ index.js
@@ -1,132 +1,139 @@
-let deck = [];
-const tipos= ["C","D","H","S"];
-const tiposEspeciales = ["A","J","Q","K"];
-let puntos = document.querySelector("small");
-const btnCarta = document.querySelector("#pedir");
-const divJugador = document.querySelector("#jugador-cartas");
-const divPc = document.querySelector("#ordenador-cartas");
-const btnNuevo = document.querySelector("#nuevo");
-let puntos1 = document.querySelector("#ia");
-const bntStop = document.querySelector("#detener");
-let puntosJugador = 0;
-    puntosIA      = 0;
-
-    // Crea la baraja de cartas
-const createDeck = ()=>{
-
-    for (let i = 2; i<= 10 ; i++){
-        for (tipo of tipos ){
-            deck.push( i +tipo)
+(()=>{
+   
+    let deck = [];
+    const tipos= ["C","D","H","S"];
+    const tiposEspeciales = ["A","J","Q","K"];
+    let puntos = document.querySelector("small");
+    const btnCarta = document.querySelector("#pedir");
+    const divJugador = document.querySelector("#jugador-cartas");
+    const divPc = document.querySelector("#ordenador-cartas");
+    const btnNuevo = document.querySelector("#nuevo");
+    let puntos1 = document.querySelector("#ia");
+    const bntStop = document.querySelector("#detener");
+    let puntosJugador = 0;
+        puntosIA      = 0;
+    
+        // Crea la baraja de cartas
+    const createDeck = ()=>{
+    
+        for (let i = 2; i<= 10 ; i++){
+            for (tipo of tipos ){
+                deck.push( i +tipo)
+            }
         }
+        for (tipo of tipos){
+            for ( especial of tiposEspeciales){
+                deck.push( especial + tipo)
+            }
+        }
+        
+        //utilizando la libreria underscore, baraja las cartas que hemos creado
+        
+        deck = _.shuffle(deck)
+        return deck
     }
-    for (tipo of tipos){
-        for ( especial of tiposEspeciales){
-            deck.push( especial + tipo)
+    createDeck()
+    
+    // Pide la baraja de cartas
+    const pedirCarta= (card) =>{
+        if (deck.lenght === 0){
+            throw ("No quedan cartas disponibles")
         }
+        let carta = deck.pop()
+        return carta;    
     }
     
-    //utilizando la libreria underscore, baraja las cartas que hemos creado
     
-    deck = _.shuffle(deck)
-    return deck
-}
-createDeck()
-
-
-// Pide la baraja de cartas
-
-
-const pedirCarta= (card) =>{
-    if (deck.lenght === 0){
-        throw ("No quedan cartas disponibles")
-    }
-    let carta = deck.pop()
-    return carta;    
-}
-
-
-const valorCarta = (carta) =>{
-    const valor = carta.substring(0,carta.length-1);
-    return ( isNaN( valor )) ?
-            (valor === "A") ? 11 : 10
-            : valor *1;
-    }
-
-    // TURNO IA
-const turnoIA = (puntosMin)=>{
-        do{
-        const carta = pedirCarta();
-
-        puntosIA = puntosIA + valorCarta(carta);
-        puntos1.textContent = puntosIA
-
-        const imgCarta = document.createElement("img")
-        imgCarta.src =`cartas/${carta}.png`
-        imgCarta.classList.add("card")
-        divPc.append(imgCarta)
-
-        if (puntosMin > 21){
-            break;
+    const valorCarta = (carta) =>{
+        const valor = carta.substring(0,carta.length-1);
+        return ( isNaN( valor )) ?
+                (valor === "A") ? 11 : 10
+                : valor *1;
         }
-
-        } while ( (puntosIA < puntosMin)  && (puntosMin <= 21));
-
-        setTimeout(()=>{
-                if (puntosIA === puntosMin){
-                    alert("Ha habido un empate")
-                }else if (puntosMin > 21){
-                    alert("La IA gana!")
-                }else if (puntosIA > 21){
-                    alert ("El jugador Gana")
-                }else {
-                    alert ("La IA gana!")
-                }
-        },100)
-
+    
+        // TURNO IA
+    const turnoIA = (puntosMin)=>{
+            do{
+            const carta = pedirCarta();
+    
+            puntosIA = puntosIA + valorCarta(carta);
+            puntos1.textContent = puntosIA
+    
+            const imgCarta = document.createElement("img")
+            imgCarta.src =`cartas/${carta}.png`
+            imgCarta.classList.add("card")
+            divPc.append(imgCarta)
+    
+            if (puntosMin > 21){
+                break;
+            }
+    
+            } while ( (puntosIA < puntosMin)  && (puntosMin <= 21));
+    
+            setTimeout(()=>{
+                    if (puntosIA === puntosMin){
+                        alert("Ha habido un empate")
+                    }else if (puntosMin > 21){
+                        alert("La IA gana!")
+                    }else if (puntosIA > 21){
+                        alert ("El jugador Gana")
+                    }else {
+                        alert ("La IA gana!")
+                    }
+            },100)
+    
+        }
+    
+//EVENTOS
+        btnCarta.onclick = ()=>{
+            const carta = pedirCarta();
+            puntosJugador = puntosJugador + valorCarta(carta)
+            puntos.textContent = puntosJugador
+        
+            const imgCarta = document.createElement("img")
+            imgCarta.src =`cartas/${carta}.png`
+            imgCarta.classList.add("card")
+            divJugador.append(imgCarta)
+        
+            if (puntosJugador > 21){
+                alert("Has perdido")
+                btnCarta.disabled= true;
+                bntStop.disabled = true;
+                turnoIA()
+            }else if (puntosJugador === 21){
+                alert("Genial!!")
+                btnCarta.disabled = true;
+                bntStop.disabled = true;
+        }
+       
     }
-
-
-
-    //EVENTOS
-
-btnCarta.onclick = ()=>{
-    const carta = pedirCarta();
-    puntosJugador = puntosJugador + valorCarta(carta)
-    puntos.textContent = puntosJugador
-
-    const imgCarta = document.createElement("img")
-    imgCarta.src =`cartas/${carta}.png`
-    imgCarta.classList.add("card")
-    divJugador.append(imgCarta)
-
-    if (puntosJugador > 21){
-        alert("Has perdido")
-        btnCarta.disabled= true;
-        bntStop.disabled = true;
-        turnoIA()
-    }else if (puntosJugador === 21){
-        alert("Genial!!")
+    bntStop.onclick = ()=>{
         btnCarta.disabled = true;
         bntStop.disabled = true;
+        turnoIA(puntosJugador)
     }
-   
-}
-bntStop.onclick = ()=>{
-    btnCarta.disabled = true;
-    bntStop.disabled = true;
-    turnoIA(puntosJugador)
-}
+    
+    
+    btnNuevo.onclick = ()=>{
+        btnCarta.disabled = false;
+        bntStop.disabled = false;
+        deck= []
+        deck = createDeck();
+        puntosJugador = 0;
+        puntosIA = 0;
+        puntos.textContent = 0;
+        puntos1.textContent = 0;
+        divJugador.textContent = ""
+        divPc.textContent = ""
+    }
+    
+})()
 
 
-btnNuevo.onclick = ()=>{
-    btnCarta.disabled = false;
-    bntStop.disabled = false;
-    deck= []
-    deck = createDeck();
-    puntosJugador = 0;
-    puntosIA = 0;
-    puntos.textContent = 0;
-    puntos1.textContent = 0;
-    divJugador.textContent = ""
-    divPc.textContent = ""
-}
+
+
+
+
+
+
+
